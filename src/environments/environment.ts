09c8ca007e98d92ce7ee1e0b503439fe56774688
Daniel-darnell <darnelldanny1997@gmail.com>
2020-10-17 21:59:54 +0300

    Rectified my header template and styled it

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key:`de7edcf2ee4735540945dbee604a6c38ded693c3`,
 };
 
 /*
